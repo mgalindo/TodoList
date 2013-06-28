87dbe8526bc49d8e00ab1a84a4b3632901cf3a0b
normanbarber <normanebarber@gmail.com>
2013-06-28 17:23:59 -0400

    including angular functionality for listitems

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -11,26 +11,29 @@ angular.module('kadaTodoApp')
 */
 
 
-function ListItemController($scope){
-    $scope.inputlistitem = 'Add item to your list';
-//    var listarray = [
-//        {
-//        'name': 'listitem1'},
-//        {'name': 'listitem2'}
-//    ];
-//    $scope.allitems = listarray;
+//function ConfirmDeleteCtrl($scope, ConfirmService)
+//{
+//    $scope.cancel = function () {
+//        ConfirmService.close();
+//    };
 
+//    $scope.doIt = function () {
+//        alert("you dirty dog");
+//        ConfirmService.close();
+//    };
+//}
+
+function ListItemController($scope){
+
+    $scope.listarray = [];
+    $scope.deleteListItem = function ( idx ) {
+            window.confirm('are you sure');
+            $scope.listarray.splice(idx, 1);
+
+    };
     $scope.addListItem = function(){
-        $scope.listarray = [];
         $scope.listarray.push( $scope.inputlistitem );
     };
 
 }
-angular.module('myApp.controllers', []).
-  controller('MyCtrl1', [function() {
-
-  }])
-  .controller('MyCtrl2', [function() {
-
-  }]);
 
